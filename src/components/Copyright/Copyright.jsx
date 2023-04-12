da19a8c0a3804f2e3e7dee556fcf0cb26e9d1ca5
Alice <102388803+Alicexplore@users.noreply.github.com>
2023-04-12 13:59:55 +0200

    fix footer

--- src/components/Copyright/Copyright.jsx
+++ src/components/Copyright/Copyright.jsx
@@ -4,7 +4,7 @@ const Copyright = () => {
   return (
     <div className='h-[40px] w-full bg-[#1b1b1b]'>
         <div className='grid grid-cols-1 text-center justify-center place-content-center font-worksans text-xs text-[#ff3d16] py-3 font-normal opacity-70'>
-            <h1 className=''>©AliceBergonhe / Design and Development / 2023</h1>
+            <h1 className=''>© AliceBergonhe / Design and Development / 2023</h1>
         </div>
     </div>
   )
